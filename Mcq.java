96483a2e5d1c7a0d24ca1707cbbf0563bb27ba7b
Rohit Singh <117452445+Rohit1234-singh@users.noreply.github.com>
2024-04-10 23:39:43 +0530

    Add files via upload

---
+++ Mcq.java
@@ -0,0 +1,34 @@
+package com.Collection;
+
+
+//      1.A
+//      2.A
+//      3.C
+//      4.D
+//      5.B
+//      6.B
+//      7.C
+//      8.C
+//      9.A
+//      10.
+//      11.C
+//      12.B
+//      13.D
+//      14.B
+//      15.A
+//      16.
+//      17.C
+//      18.D
+//      19.B
+//      20.
+
+
+
+public class Mcq {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
